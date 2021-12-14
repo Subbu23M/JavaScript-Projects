171905672c8ad9c8681c19a929d570e5875c6e5c
subramanyam <subbuvenkat2495@gmail.com>
2021-12-14 14:22:39 +0530

    new-file-created

---
+++ Count-Down-Project/JavaScript/main.js
@@ -0,0 +1,32 @@
+// Declared & Assigned
+const months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'
+];
+
+// Declared & Assigned
+const weekDays = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday'
+];
+
+// Select the elements
+const giveawayText = document.querySelector('.giveaway');
+
+const item = document.querySelectorAll('.deadline h4');
+console.log(item);
